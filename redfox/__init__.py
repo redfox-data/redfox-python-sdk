a711fcbde85fe7632ab91ec6b3b018504bac7e43
yuanyi <1533804761@qq.com>
2026-08-31 15:26:54 +0800

    chore: update version to 0.4.0 in setup.py and __init__.py

--- redfox/__init__.py
+++ redfox/__init__.py
@@ -3,7 +3,7 @@
 from .client import RedFoxClient, AsyncRedFoxClient
 from .exceptions import RedFoxAPIError, RedFoxAuthError, RedFoxRateLimitError
 
-__version__ = "0.2.2"
+__version__ = "0.4.0"
 __all__ = [
     "RedFoxClient",
     "AsyncRedFoxClient",
